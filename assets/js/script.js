a9167c25d299266a490a6e32ce2bceb08603c350
nfailor <nfailor@gmail.com>
2023-10-20 17:18:34 -0500

    added additional IDs to index.html; removed omdb api to include tmdb api, updated the movie search function, the movie modal function, and added youtube video embedding to the modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,25 @@
 // nick youtube api key: AIzaSyBWrhCB3guqUxUZxZX8HvCsGdb9BwsCyU0
-// nick omdb api key: 261cdc84
+// nick tmdb api key: 2ed376b49026ee5ba769954152774c28
 // madison youtube api key:
-// Send all data requests to:
-// http://www.omdbapi.com/?apikey=[yourkey]&
-// Poster API requests:
-// http://img.omdbapi.com/?apikey=[yourkey]&
 
-var omdbAPI = "261cdc84";
+var tmdbAPI = "2ed376b49026ee5ba769954152774c28";
 var ytAPI = "AIzaSyBWrhCB3guqUxUZxZX8HvCsGdb9BwsCyU0";
 var movieSearchButton = document.getElementById("search-button");
 var movieInput = document.getElementById("movie-input");
 var movieContainer = document.getElementById("movie-container");
-var modalLink = document.getElementById("modal-link");
+var modalVideo = document.getElementById("video-container");
 var modalCard = document.getElementById("modal-card");
-var movieTitle = document.getElementById("modal-title");
+var movieTitle = document.getElementById("movie-title");
 var moviePlot = document.getElementById("movie-plot");
 
-// Functions to open and close a modal
+// Function to open a modal
 function openModal($el) {
   $el.classList.add("is-active");
 }
 
+// Function to close a modal
 function closeModal($el) {
   $el.classList.remove("is-active");
-}
-
-function closeAllModals() {
-  (document.querySelectorAll(".modal") || []).forEach(($modal) => {
-    closeModal($modal);
-  });
 }
 
 // Add a click event on buttons to open a specific modal
@@ -61,69 +52,56 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
-// modalLink.addEventListener("click", function () {
+function modalDisplay(modalData) {
+  // Update the "movie-title" element with the retrieved title
+  movieTitle.innerText = modalData.title;
 
-//     var youtubeLink = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + movie + "&key=" + ytAPI;
+  // Check if the plot is already available in modalData
+  if (modalData.plot) {
+    // Update the "movie-plot" element with the retrieved plot
+    moviePlot.innerText = modalData.plot;
+  } else {
+    // If plot is not available in modalData, you can fetch it here
+    var searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${tmdbAPI}&query=${modalData.title}`;
 
-//     fetch(youtubeLink)
-//     .then(function (response) {
-//       console.log(response);
-//       if (!response.ok) {
-//         throw new Error("Network response was not ok");
-//       }
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//     });
-// });
+    fetch(searchUrl)
+      .then(function (response) {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(function (searchData) {
+        if (searchData.results.length > 0) {
+          var firstResult = searchData.results[0];
+          var plot = firstResult.overview;
+          // Update the "movie-plot" element with the retrieved plot
+          moviePlot.innerText = plot;
 
-function modalDisplay(movieData) {
-    var movie = movieData.Title;
-    movieTitle.innerText = "";
-    movieTitle.innerText = movieData.title
-
-    moviePlotShort = "https://www.omdbapi.com/?apikey=" + omdbAPI + "&t=" + movie;
-
-
-    // not complete - only pulls initial search plot
-    fetch(moviePlotShort)
-        .then(function (response) {
-            console.log(response);
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then(function (data) {
-            var moviePlotShort = data.Plot;
-            moviePlot.innerText = "";
-            moviePlot.innerText = moviePlotShort;
-        })
-        .catch(function (error) {
-            console.error("Error fetching movie data:", error);
-        });
-
-    
-};
+          // Update the plot in modalData for future reference
+          modalData.plot = plot;
+        } else {
+          // Handle the case where no results were found
+          moviePlot.innerText = "No plot information available.";
+        }
+      })
+      .catch(function (error) {
+        console.error("Error fetching movie data:", error);
+      });
+  }
+}
 
 movieSearchButton.addEventListener("click", function () {
   // user's input from search
-  var movie = movieInput.value;
+  var movieInputValue = movieInput.value;
 
   //clears search bar after click
   movieInput.value = "";
 
-  var searchedMovie =
-    "https://www.omdbapi.com/?apikey=" +
-    omdbAPI +
-    "&s=" +
-    movie +
-    "&page=10&y=&r=json";
+  var searchedMovie = `https://api.themoviedb.org/3/search/movie?api_key=${tmdbAPI}&query=${movieInputValue}`;
 
   fetch(searchedMovie)
     .then(function (response) {
-      console.log(response);
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
@@ -131,59 +109,123 @@ movieSearchButton.addEventListener("click", function () {
     })
     .then(function (data) {
       console.log(data);
-
       movieContainer.innerHTML = "";
 
-      data.Search.forEach(function (data) {
-        var movieTitle = data.Title;
-        var moviePlot = data.Plot;
-        var moviePoster = data.Poster;
+      data.results.forEach(function (data) {
+        var movieTitle = data.title;
+        var moviePoster = data.poster_path;
 
-        var modalDataOBJ = {
-            title: movieTitle,
-            plot: moviePlot,
-            // video: ytEmbed,
-        }
+        var movieDiv = document.createElement("div");
+        movieDiv.classList.add("col-md-4");
 
-        var movieDiv = document.createElement("div")
+        var posterBaseURL = "https://image.tmdb.org/t/p/w200";
+        var moviePosterURL = posterBaseURL + moviePoster;
+
+        var modalData = {
+          title: movieTitle,
+          plot: "", // Initialize plot as empty
+        };
+
+        movieDiv.setAttribute("data-modal-title", modalData.title);
+        movieDiv.setAttribute("data-modal-plot", modalData.plot);
 
         var movieCard = `
-                    <div class="col-md-2 movie-card">
-                        <div class="card">
-                            <div class="card-header js-modal-trigger" data-target="modal-card">${movieTitle}</div>
-                            <div class="card-body">
-                                <p class="moviePoster"> <img src="${moviePoster}" alt="${movieTitle} Poster"></p>
-                            </div>
-                        </div>
-                    </div>
-                `;
+          <div class="movie-card" data-modal-data='${JSON.stringify(
+            modalData
+          )}'>
+            <div class="card">
+              <div class="card-header js-modal-trigger" data-target="modal-card">${movieTitle}</div>
+              <div class="card-body">
+                <p class="moviePoster"> <img src="${moviePosterURL}" alt="${movieTitle} Poster"></p>
+              </div>
+            </div>
+          </div>
+        `;
 
-        
         movieDiv.innerHTML = movieCard;
-
         movieContainer.appendChild(movieDiv);
 
-        movieDiv.addEventListener("click", function(event) {
-            console.log(event);
-            if (event.target.classList.contains("js-modal-trigger")) {
-                modalDisplay(modalDataOBJ);
-            }
-        })
+        movieDiv.addEventListener("click", function (event) {
+          if (event.target.classList.contains("js-modal-trigger")) {
+            var modalTitle = this.getAttribute("data-modal-title");
+            var modalPlot = this.getAttribute("data-modal-plot");
 
+            // Create the modalData object with title and plot
+            var modalData = {
+              title: modalTitle,
+              plot: modalPlot,
+            };
+
+            // Pass the modalData to the modalDisplay function
+            modalDisplay(modalData);
+
+            // Fetch and update the plot data
+            var searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${tmdbAPI}&query=${modalData.title}`;
+
+            fetch(searchUrl)
+              .then(function (response) {
+                if (!response.ok) {
+                  throw new Error("Network response was not ok");
+                }
+                return response.json();
+              })
+              .then(function (searchData) {
+                if (searchData.results.length > 0) {
+                  var firstResult = searchData.results[0];
+                  modalData.plot = firstResult.overview;
+                  // Update the "movie-plot" element with the retrieved plot
+                  moviePlot.innerText = modalData.plot;
+                } else {
+                  // Handle the case where no results were found
+                  modalData.plot = "No plot information available.";
+                  // Update the "movie-plot" element with the message
+                  moviePlot.innerText = modalData.plot;
+                }
+              })
+              .catch(function (error) {
+                console.error("Error fetching movie data:", error);
+              });
+            
+              var youtubeLink = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${movieTitle}&key=${ytAPI}&maxResults=1`;
+
+              console.log("Youtube API Request:", youtubeLink);
+          
+              fetch(youtubeLink)
+              .then(function (response) {
+                if (!response.ok) {
+                  throw new Error("Network response was not ok");
+                }
+                return response.json();
+              })
+              .then(function (data) {
+                // Assuming you want the first video from the search results
+                if (data.items && data.items.length > 0) {
+                  var videoId = data.items[0].id.videoId;
+                  
+                  // Create an iframe to embed the video
+                  var iframe = document.createElement('iframe');
+                  iframe.src = `https://www.youtube.com/embed/${videoId}`;
+                  iframe.setAttribute("allowfullscreen", "allowfullscreen");
+          
+                  // Add the iframe to your modal or wherever you want to display the video
+                  // Example: modalCard.appendChild(iframe);
+                  document.getElementById("video-container").innerHTML = "";
+          
+                  document.getElementById("video-container").appendChild(iframe);
+          
+                } else {
+                  // Handle the case where no video results were found
+                  console.log("No video found for this movie.");
+                }
+              })
+              .catch(function (error) {
+                console.error("Error fetching video data:", error);
+              });
+            
+            // Display the modal
+            openModal(modalCard);
+          }
+        });
       });
-
-      (document.querySelectorAll(".js-modal-trigger") || []).forEach(
-        ($trigger) => {
-          const modal = $trigger.dataset.target;
-          const $target = document.getElementById(modal);
-
-          $trigger.addEventListener("click", () => {
-            openModal($target);
-          });
-        }
-      );
-    })
-    .catch(function (error) {
-      console.error("Error fetching movie data; ", error);
     });
 });
