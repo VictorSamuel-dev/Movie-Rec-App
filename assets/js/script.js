963b2c9be7f9edf99159a25762782dc7054e5c52
VictorSamuel-dev <142364059+VictorSamuel-dev@users.noreply.github.com>
2023-10-22 15:33:24 -0400

    modified changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
 // nick youtube api key: AIzaSyBWrhCB3guqUxUZxZX8HvCsGdb9BwsCyU0
 // nick tmdb api key: 2ed376b49026ee5ba769954152774c28
-<<<<<<< HEAD
 // madison youtube api key:
 // victor youtube api key: AIzaSyDjoFhFJosMUmPbXNBr0L87PtX4_I4PJYk
 //victor tmdb api key: f1af2709945a3588fa0ae7c5d3f25da8
-=======
 // madison youtube api key:AIzaSyBOVuYX_z7sZzI8Ehjp-EJIUBd2qdpztK0
->>>>>>> 77baae8a922ec690d631c97a39012b00ba18ba54
 
 
 var tmdbAPI = "2ed376b49026ee5ba769954152774c28";
